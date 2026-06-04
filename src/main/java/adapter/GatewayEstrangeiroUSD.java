3613aa6ec53078187717a51a3af1a91785386a64
Mateus Ramos <mateusramosv@hotmail.com>
2026-06-04 11:27:37 -0300

    Criando o mock para o projeto

---
+++ src/main/java/adapter/GatewayEstrangeiroUSD.java
@@ -0,0 +1,14 @@
+package adapter;
+
+public class GatewayEstrangeiroUSD {
+
+    private double valorUSD;
+
+    public double getValorUSD() {
+        return valorUSD;
+    }
+
+    public void setValorUSD(double valorUSD) {
+        this.valorUSD = valorUSD;
+    }
+}
